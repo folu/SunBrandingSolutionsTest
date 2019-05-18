557d23cc09f3fa384b8bf282c4751d5e8248c9d8
folu <f0lu@hotmail.co.uk>
2019-05-18 19:43:10 +0100

    project complete

---
+++ SunBrandingSolutionsTest/Controllers/ApiClient.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using SunBrandingSolutionsTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SunBrandingSolutionsTest.Controllers
+{
+    public class ApiClient
+    {
+        private const string BaseURL = "https://coding-interview.azurewebsites.net/api/asset/";
+        private const string Code = "?code=RDlFQJ0KDI3GbwU6GqoZ/tukFcGGF21lZR8PhiECkLRjIElGaMoafw";
+
+        private HttpClient client;
+
+        public ApiClient ()
+        {
+            client = new HttpClient();
+        }
+        public async Task<ApiAsset> GetAsset (int id)
+        {
+            string url = BaseURL + id + Code;
+            var response = await client.GetAsync(new Uri(url));
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                ApiAsset apiAsset = JsonConvert.DeserializeObject<ApiAsset>(result);
+                return apiAsset;
+            }
+            return null; 
+        }
+    }
+}
---
+++ SunBrandingSolutionsTest/Models/ApiAsset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunBrandingSolutionsTest.Models
+{
+    public class ApiAsset
+    {
+        public int AssetId { get; set; }
+        public IList<Annotation> Annotations { get; set; }
+        public string Asset { get; set; }
+    }
+
+    public class Annotation
+    {
+        public int AnnotionId { get; set; }
+        public string Annotator { get; set; }
+        public string Contents { get; set; }
+    }
+}
